946972cca12cb0b9d35293ca289385d0aa0aa11a
Mukho <mukho@kakao.com>
2026-09-25 22:38:33 +0900

    feat(P1-03): configure MySQL JPA DataSource and BaseTimeEntity with auditing

---
+++ src/main/java/com/mukplay/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.mukplay.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
