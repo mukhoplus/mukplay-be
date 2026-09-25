6fb90a6560577dde94d985c05b72fe9eb22cb4ba
Mukho <mukho@kakao.com>
2026-09-25 22:15:32 +0900

    feat(P1-01): bootstrap Spring Boot 4.1.1 project with Java 25 and Gradle

---
+++ src/test/java/com/mukplay/MukplayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukplayBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
